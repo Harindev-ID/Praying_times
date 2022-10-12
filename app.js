c28b5b0fdb27dcce7573f7c85c2e4afaf266f6db
adityadenny <adityadenny6@gmail.com>
2022-10-12 21:11:54 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function index(){
+    let app         = document.getElementById('app');
+    let h3          = document.createElement('h3');
+    h3.innerHTML    = 'Prayers Times';
+
+    app.appendChild(h3);
+}
