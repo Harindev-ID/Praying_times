d0cb1e7839c250af06a0861e614e901a37abb21e
adityadenny <adityadenny6@gmail.com>
2022-10-12 21:23:58 +0700

    permission geolocation

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+function success(position){
+    console.log(position);
+}
+
+function error(){
+    alert('Your location not accessible');
+}
+
+function userlocation(){
+    if(!navigator.geolocation){
+        alert('Geolocation not support in your browser, Please use another browser')
+    } else{
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
 function index(){
     let app         = document.getElementById('app');
     let h3          = document.createElement('h3');
     h3.innerHTML    = 'Prayers Times';
 
     app.appendChild(h3);
+
+    userlocation();
 }
 
 index();
