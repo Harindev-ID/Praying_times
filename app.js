39d2bf4a65506c10f77fa1f5b19384b68caa1d50
adityadenny <adityadenny6@gmail.com>
2022-10-12 22:18:05 +0700

    create table prayer times

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ function prayerTimes(latitude, longitude){
     fetch('https://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=2&month=10&year=2022')
     .then(Response => Response.json())
     .then(function(Response){
-        let date    = new Date();
-        let today   = date.getDate() - 1;
-        // console.log(today);
-        // console.log(Response.data[today]);
+        let date        = new Date();
+        let today       = date.getDate() - 1;
+        let data        = Response.data[0].timings;
+
+        let app         = document.getElementById('app');
+        let table       = document.createElement('table');
+        let tableTbody  = document.createElement('tbody');
+
+        for(i in data){
+            let row         = tableTbody.insertRow();
+            let name        = row.insertCell(0);
+            let time        = row.insertCell(1);
+            name.innerHTML  = i;
+            time.innerHTML  = data[i];
+            tableTbody.appendChild(row);
+        }
+        table.appendChild(tableTbody);
+        app.appendChild(table);
     });
 }
 
